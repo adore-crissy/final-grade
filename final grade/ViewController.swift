62660ff1190f2ab16ad57d75e4bc54a2c12481ce
adore-crissy <68021411+adore-crissy@users.noreply.github.com>
2020-07-18 00:27:56 -0500

    this is the final grade project

--- final grade/ViewController.swift
+++ final grade/ViewController.swift
@@ -9,12 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+@IBOutlet weak var currentGradeTextFeild: UITextField!
+    
+@IBOutlet weak var desiredGradeTextFeild: UITextField!
+    
+@IBOutlet weak var finalWorthTextFeild: UITextField!
+    
+@IBAction func gradeNeededBtn(_ sender: Any) {
+let currentGrade = Double(currentGradeTextFeild.text!)
+let desiredGrade = Double(desiredGradeTextFeild.text!)
+let finalWorthPercentage = Double(finalWorthTextFeild.text!)
+    
+let oneHundredPercent = 100.0
+let percentageOfGrade = Double(oneHundredPercent - finalWorthPercentage!)/100.0
+    
+let gradeNeededOnFinal = (desiredGrade! - (percentageOfGrade * currentGrade!))/finalWorthPercentage!
+    
+gradeNeededLabel.text = "You Need \(gradeNeededOnFinal * 100)%"
+    
+}
+    @IBOutlet weak var gradeNeededLabel: UILabel!
+    
+    
+       
+    
 }
 
